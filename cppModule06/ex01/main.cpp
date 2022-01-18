70da731ccef29f1e3d51a09e549ca699e1387080
rloyce <delakruel@gmail.com>
2022-01-18 15:03:25 +0300

    small add in main, nvm

--- cppModule06/ex01/main.cpp
+++ cppModule06/ex01/main.cpp
@@ -12,11 +12,14 @@ int main() {
 	Data	testData;
 	testData.num = 20;
 	testData.str = "kekis";
+	Data	*testData2;
 
-	std::cout << &testData << " is 1st Data adress" << std::endl;
+	std::cout << &testData << " is Data adress" << std::endl;
 	std::cout << serialize(&testData) << " is serialized Long" << std::endl;
 	std::cout << deserialize(serialize(&testData)) << " is deserialized serialazed Data adress" << std::endl;
 	std::cout << testData.num << " " << testData.str << " is 1st Data params" << std::endl;
 	std::cout << deserialize(serialize(&testData))->num << " " << deserialize(serialize(&testData))->str << " is deserialized serialazed Data params" << std::endl;
-
+	testData2 = deserialize(serialize(&testData));
+	std::cout << testData2 << " is Data2 adress" << std::endl;
+	std::cout << testData2->num << " " << testData2->str << " is Data2 params" << std::endl;
 }
